52132f91e5a2414916dea0bef0b4775da7bbcb39
longkb <longkb@bka.vn>
2016-10-19 01:57:53 +0900

    Demo git Eclipse

--- testEgit/src/testEgit/egit.java
+++ testEgit/src/testEgit/egit.java
@@ -1,7 +1,7 @@
-package helloworld;
+package testEgit;
 
 public class egit {
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		System.out.println("Helloworld");
 	}
 }
